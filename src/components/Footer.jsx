79642bb15bd482ada19f57e276570bdee492e880
shishupaltech <veerutech75@gmail.com>
2023-04-16 06:58:47 +0530

    project is deleted

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,38 @@
-import { Avatar, Box, Stack, Text, VStack } from '@chakra-ui/react'
+import { Avatar, Box, Stack, Text, VStack,Heading,Button } from '@chakra-ui/react'
 import React from 'react'
 import avtar_img from '../assets/avtar_img.jpeg'
+import {AiOutlineSend} from 'react-icons/ai';
 
 const Footer = () => {
   return (
     <Box bgColor={"blackAlpha.900"} color={"whiteAlpha.700"} minH={"48"} PX={"16"} PY={["16","8"]}>
-        <Stack direction={["column","row"]} h={"full"} alignItems={"center"}>
-            <VStack ml={["4","3"]} w={"full"} alignItems={["center","flex-start"]}>
-                <Text fontWeight={"bold"}>About Us</Text>
-                <Text  fontSize={"sm"} letterSpacing={"widest"} textAlign={["center","left"]}>We are the best crypto trading app in India, we provide our guidance at a very cheap price.</Text>
-            </VStack>
-            <VStack>
-                <Avatar boxSize={"28"} mt={["4","2"]} mr={["4","4"]} src={avtar_img}/>
-                <Text>Our Footer</Text>
-            </VStack>
-        </Stack>
+       <Stack direction={["column","row"]} h={"full"} alignItems={"center"}>
+  <VStack ml={["4","3"]} w={"full"} alignItems={["center","flex-start"]} borderRight="4px solid white">
+    <Text fontWeight={"bold"}>About Us</Text>
+    <Text fontSize={"sm"} letterSpacing={"widest"} textAlign={["center","left"]}>
+      "Cryptocurrency represents a new and innovative technology that has the potential to disrupt traditional financial systems and create new opportunities for decentralized transactions and value exchange."
+    </Text>
+  </VStack>
+  <VStack ml={["4","1"]} w={"full"} h={"full"} alignItems={["center","flex-start"]} borderRight={["none","4px solid white"]}>
+  <Heading size={"md"} textTransform={"uppercase"} colorScheme={"whiteAlpha"}>
+                    Social Media
+                </Heading>
+                <Button variant={'link'} colorScheme={"white"}>
+                    <a href="https://youtube.com/codefriendy" target={'blank'}>Youtube</a>
+                </Button>
+                <Button variant={'link'} colorScheme={"white"}>
+                    <a href="https://instagram.com/shishupal7169" target={'blank'}>Instagram</a>
+                </Button>
+                <Button variant={'link'} colorScheme={"white"} target={"blank"}>
+                    <a href="https://github.com/shishupaltech">github</a>
+                </Button>
+  </VStack>
+  <VStack>
+    <Avatar boxSize={"28"} mt={["4","2"]} mr={["4","4"]} src={avtar_img}/>
+    
+  </VStack>
+</Stack>
+
     </Box>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,30 @@
-import { Box, Image ,Text} from '@chakra-ui/react'
+import { Box, Image ,Text, Heading, Container,Stack} from '@chakra-ui/react'
 import React from 'react';
-import img1 from '../assets/img.png';
-
-
-
+import img from '../assets/img.png';
 import { motion } from "framer-motion";
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import img1 from '../assets/img1.jpg';
+import img2 from '../assets/img2.jpg';
+import img3 from '../assets/img3.jpg';
+import img4 from '../assets/img4.jpg';
+import img5 from '../assets/img5.webp';
+const headingOptions = {
+  pos:'absolute',
+  left:"50%",
+  top:"50%",
+  transform:"translate(-50%,-50%)",
+  textTransform:"uppercase",
+  padding:"4",
+  size:"4xl"
+}
 
 const Home = () => {
   return (
-    <Box bgColor={"blackAlpha.900"} w={"full"} h={"85vh"} alignItems={"center"}>
+    <>
+    <Box >
+        <MyCarousel/>
+        <Box bgColor={"black.900"} w={"full"} h={"85vh"} alignItems={"center"}>
       <motion.div
         style={{
           height: "80vh",
@@ -26,7 +42,7 @@ const Home = () => {
           w={"full"}
           h={"400"}
           objectFit={"contain"}
-          src={img1}
+          src={img}
           filter={"grayscale(1)"}
           
         />
@@ -42,7 +58,61 @@ const Home = () => {
         Xcrypto
       </Text>
     </Box>
+        <Container maxW={'container.xl'} minH={"100vh"} p="16">
+          <Heading textTransform={'uppercase'} py="2" w={"fit-content"} borderBottom={"2px solid"} m="auto" >
+            Crypto Currency ?
+          </Heading>
+        <Stack
+          h="full"
+          p={"4"}
+          alignItems={"center"}
+          direction={["column","row"]}
+        >
+          <Image src={img5} h={['40','250']} filter={"hue-rotate(-130deg)"}  borderRadius="20px"/>
+          <Text letterSpacing={'widest'} lineHeight={"190%"} p={['4','16']}>
+          Cryptocurrency is a digital or virtual currency that uses cryptography for security and operates independently of a central bank. Cryptocurrencies use decentralized systems, such as blockchain technology, to manage and record transactions. Some of the most popular cryptocurrencies include Bitcoin, Ethereum, Litecoin, and Ripple. 
+
+While cryptocurrency has the potential to provide greater financial freedom and security, it also carries risks, such as volatility and security concerns. It's important to do your own research and understand the risks before investing in or using cryptocurrencies.
+          </Text>
+        </Stack>
+        </Container>
+    </Box>
+    
+    </>
   );
 };
+
+const MyCarousel = ()=>(
+  <Carousel autoPlay infiniteLoop interval={1000} showStatus={false} showArrows={false} showThumbs={false}>
+      <Box w="full" h={'100vh'}>
+    <Image src={img1} h="full" w={'full'} objectFit={'cover'} />
+    <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
+    Do your research:
+    </Heading>
+  </Box>
+  <Box w="full" h={'100vh'}>
+    <Image src={img2} h="full" w={'full'} objectFit={'cover'} />
+    <Heading bgColor={'whiteAlpha.900'} color={'black'} {...headingOptions}>
+    Diversify your portfolio
+    </Heading>
+  </Box>
+
+  <Box w="full" h={'100vh'}>
+    <Image src={img3} h="full" w={'full'} objectFit={'cover'} />
+    <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+    Have a long-term strategy
+    </Heading>
+  </Box>
+
+  <Box w="full" h={'100vh'}>
+    <Image src={img4} h="full" w={'full'} objectFit={'cover'} />
+    <Heading bgColor={'whiteAlpha.600'} color={'white'} {...headingOptions}>
+    Use a secure wallet
+    </Heading>
+  </Box>
+      
+      
+  </Carousel>
+);
 
 export default Home;
